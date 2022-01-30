c950547a2a604bc5f92a85c4cfe023d41f7bb3b0
Meenu Maria Mathews <38442902+meenu-mathews@users.noreply.github.com>
2022-01-30 20:32:40 +0000

    Initial user inputs for rover's position User inputs for moving rover Checking instructions using if

--- src/com/marsrover/app/Main.java
+++ src/com/marsrover/app/Main.java
@@ -1,4 +1,5 @@
 package com.marsrover.app;
+import com.marsrover.model.MarsRover;
 import com.marsrover.model.Plateau;
 
 import java.util.Scanner;
@@ -8,11 +9,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scannerObj = new Scanner(System.in);
         System.out.println("Enter max coordinates of plateau");
-
         int xCoord = scannerObj.nextInt();
         int yCoord = scannerObj.nextInt();
-
         Plateau plateauObj = new Plateau(xCoord,yCoord);
+
+
+
+
     }
 }
 
--- src/com/marsrover/model/Plateau.java
+++ src/com/marsrover/model/Plateau.java
@@ -1,10 +1,33 @@
 package com.marsrover.model;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Plateau {
+
 
     public Plateau(int xCoord, int yCoord) {
 
-        int[][] grid = new int[xCoord][yCoord];
+        String[][] grid = new String[xCoord][yCoord];
         System.out.println("["+xCoord+"]["+yCoord+"] grid created");
+
+        System.out.println("Enter rover's coordinates and direction (N,S,W,E)");
+        Scanner scannerObj = new Scanner(System.in);
+        int xCoordRover = scannerObj.nextInt();
+        int yCoordRover = scannerObj.nextInt();
+        char directionRover = scannerObj.next(".").charAt(0);
+        grid[xCoordRover][yCoordRover] = "Rover present";
+
+        System.out.println(grid[xCoordRover][yCoordRover] + " at ["+xCoordRover+"]["+yCoordRover+"] facing "+directionRover);
+
+        System.out.println("Enter instructions (L,R,M) to move the Rover around the Plateau ");
+        String roverMove = scannerObj.nextLine();
+        
+        for(int i = 0; i<roverMove.length(); i++) {
+
+            char instruction = roverMove.toUpperCase(Locale.ROOT).charAt(i);
+            if(instruction == 'L')
+            System.out.print(instruction + ", ");
+        }
     }
 }
